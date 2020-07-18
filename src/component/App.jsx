73025fdb4ba3c14150dc3c1d86c056b56b7ab005
vy-hoang <57076819+vy-hoang@users.noreply.github.com>
2020-07-17 22:13:30 -0400

    to-do list style

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Date from "./date";
+import List from "./todo-list";
 
 
 function App() {
+  
+  
   return (
-    <h1>hello</h1>
+    <div>
+      <List />
+      <div className="Calender"><h1 className="float">Calender</h1></div>
+      <div className="calender-box">
+        <div className="grid">
+        {Array.from(Array(31), (_, i) => i + 1).map((num) => {
+            return(
+              <Date 
+               
+                number={num}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default App;
