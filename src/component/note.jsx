965dad7c035520ad7a47a43f4251c44b4a7b7ba2
vy-hoang <57076819+vy-hoang@users.noreply.github.com>
2020-07-18 16:17:19 -0400

    to-do list is completed

--- src/component/note.jsx
+++ src/component/note.jsx
@@ -1,12 +1,43 @@
 import React, { useState } from 'react';
 
 function Note(props){
+    const [line, setLine] = useState(false);
+    const [over, setOver] = useState(false);
+   
+    function Cross(){
+        setLine(preValue =>{
+            return !preValue;
+        });
+    }
+    function Over(){
+        setOver(true);
+    }
+    function Out(){
+        setOver(false);
+    }
+    function Delete(){
+
+        props.onDelete(props.id);
+    }
     return(
 
         <div>
-            <ul>
-                <li> {props.content}<span><i class="fa fa-trash-o bin" /></span></li>
-            </ul>
+            
+               
+                <ul className="padding">
+                     <li  style={{textDecoration: line ? "line-through": "none"}}> 
+                        <span onClick={Cross}>{props.content} </span>
+                        <span><i class="fa fa-trash-o bin" onMouseOver={Over} onMouseOut={Out}
+                        style={{color: over ? "red": "black"}}
+                        onClick={Delete}
+                    /></span>
+                    </li>
+                    
+                </ul>
+               
+                    
+              
+            
         </div>
 
     );
--- src/component/todo-list.jsx
+++ src/component/todo-list.jsx
@@ -23,7 +23,23 @@ function List(){
         setItem("");
     }
 
+    function Remove(id){
+       
     
+
+        if(window.confirm("delete this item")){
+            setArray(PreDel => {
+           
+                return PreDel.filter((list, index) => {
+                   
+                    return index !== id;
+                   
+                });
+            });
+        }else{
+            return array;
+        }
+    }
 
     return(
 
@@ -37,10 +53,13 @@ function List(){
                     <button onClick={handleClick} type="submit"> <span >Add</span></button>
                     
                 </div>
-                {array.map((list) =>{
+                {array.map((list, index) =>{
                     return(
                         <Note 
                             content={list} 
+                            onDelete={Remove}
+                            key={index}
+                            id={index}
                         />
                     );
                     })}
